c8b1d91af1c8073c6bb9ef46d72c00f588dfd580
Sillynarwhals <blewetta25@greenhill.org>
2022-06-01 12:51:26 -0500

    bodymassindex

---
+++ addtwonumbers.py
@@ -0,0 +1,13 @@
+# Asha Blewett
+# add two numbers together
+import os
+os.system ('cls')
+
+number1 = int(input("pick a number: "))
+number2 = int(input("pick a number: "))
+add = number1 + number2
+print(add) # displaying what the two number equal to
+print(number1 + number2)
+
+print ("The first number is", number1, end = " ")
+print ("The second number is", number2)
---
+++ bodymassindex.py
@@ -0,0 +1,12 @@
+# Asha Blewett
+# calculate a persons BMI
+
+import os
+os.system ('cls')
+
+weight = int(input("your weight in kg: ")) 
+weight2= weight * weight 
+height = int(input("your height in meters: "))
+BMI= weight2/height 
+
+print ("Your BMI is: ", BMI) 
