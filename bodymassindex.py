113fb7a606ff2ccd60c9700ebabcefda2d614623
Sillynarwhals <blewetta25@greenhill.org>
2022-06-02 12:11:36 -0500

    jello

--- bodymassindex.py
+++ bodymassindex.py
@@ -10,3 +10,12 @@ height = int(input("your height in meters: "))
 BMI= weight2/height 
 
 print ("Your BMI is: ", BMI) 
+
+if (BMI < 14.5):
+    print ("you're underweight")
+
+if (BMI > 40):
+    print ("you're overweight")
+
+if (BMI > 14.5) or (BMI < 40):
+    print ("you're healthy")
