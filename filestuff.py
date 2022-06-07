61f025702ee384fd3d08a320a8b3c523b4675e74
Sillynarwhals <blewetta25@greenhill.org>
2022-06-07 14:59:35 -0500

    choice list game

---
+++ filestuff.py
@@ -0,0 +1,37 @@
+#Asha Blewett
+#Prgram abt using File, 
+# 'r' read 
+# 'a' append 
+# 'w' write open a file and make sure you close it 
+# datetime
+import random
+import os, datetime
+
+os.system('cls')
+
+date=datetime.datetime.now()
+print(date)
+print(date.strftime("%m-%d-%Y"))
+
+name="Asha"
+sce=344
+scrLine=str(sce)+"\t"+date.strftime("%m-%d-%Y")+ "\t"+name +"\n"
+print(scrLine)
+#create a file 
+myFile = open("scre.txt", 'w')
+myFile.write(scrLine)
+myFile.close()
+#Create new line
+name="Peter"
+sce=132
+scrLine=str(sce)+"\t"+date.strftime("%m-%d-%Y")+ "\t"+name +"\n"
+#Append to your file add lines the file
+myFile = open("scre.txt", 'a')
+myFile.write(scrLine)
+myFile.close()
+#Read the file 
+myFile = open("scre.txt", 'r')
+stuff=myFile.readlines()
+myFile.close()
+for line in stuff:
+    print(line)
--- listgame.py
+++ listgame.py
@@ -1,5 +1,8 @@
 # Asha Blewett
-# 
+# guessing game for colors
+#my colors= pink, red, orange, yellow, green, blue, purple, brown, black, white
+from ast import Break
+from audioop import add
 import os
 import random
 os.system ('cls')
@@ -8,25 +11,52 @@ for i in range (74):
     print ("*", end = "")
 print ()
 print ( "|----------------------- Color Wheel ------------------------------------|")
-print ("| The goal of this game is to guess the random color chosen from a list  |")
-print ("|If you guess correct you win. If you guess incorrect reset and try again|")
-print ("|                         good luck ^v^                                  |")
+print ("| 1. Animal                                                               |")
+print ("| 2. Fruit                                                                |")
+print ("| 3. Color                                                                |")
+print ("|If you guess correct you win. If you guess incorrect reset and try again |")
+print ("|                         good luck ^v^                                   |")
 for i in range (74):
     print ("*", end = "")
 MyColors= ["pink", "red", "orange", "yellow", "green", "blue", "purple", "brown", "black", "white"]
+MyFruit= ["apple", "banana", "orange", "grapefruit", "blueberry", "mango", "strawberry", "rasberry", "watermelon","blackberry"]
+MyAnimal= ["dog", "cat", "snake", "cow", "pig", "chicken", "fish","bear", "ant", "turtle"]
 print ()
-for i in range (1):
-    element= random.choice(MyColors)
+count=0
+Game=True
 
-
-
+while True:
+    choice= input("What game would you like to play 1, 2, or 3? ")
+    try:
+        choice=int(choice)
+        if choice > 0 and choice < 4:
+            break
+        else:
+            print("give me 1,2, or 3")
+    except:
+        print("Please enter a number")   
+    
+def selectWord(choice):
+    global element
+    if choice==1: 
+        element= random.choice(MyColors)
+    if choice==2:
+        element=random.choice(MyFruit)
+    if choice==3:
+        element=random.choice(MyAnimal)
+    
 for i in range (10):
-    guess= input ("input a color:")
+    guess= input ("Please put your guess here:")
 
-    if guess== element:
+    if guess== random.choice(MyColors):
+        print ("Congrats! You won :)")
+        break
+    if guess== random.choice(MyFruit):
+        print ("Congrats! You won :)")
+        break
+    if guess== random.choice(MyAnimal):
         print ("Congrats! You won :)")
         break
     else:
         print ("f Try again!")
         continue
-
